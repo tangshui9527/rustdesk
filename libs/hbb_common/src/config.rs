04951d21c61ad380ef36e811233bd6fdbefddb4c
tangshui9527 <108613817+tangshui9527@users.noreply.github.com>
2024-11-21 20:47:10 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -101,7 +101,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["tangshui.fun"];
-pub const PUBLIC_RS_PUB_KEY: &str = "Et6jhS4x69dFOwr0jaXGNmF74nfIOeSB1oAcSHXKeU4="
+pub const PUBLIC_RS_PUB_KEY: &str = "Et6jhS4x69dFOwr0jaXGNmF74nfIOeSB1oAcSHXKeU4=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
